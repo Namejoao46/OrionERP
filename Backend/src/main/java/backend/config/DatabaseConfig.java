b113d181130e34dc1208fbfc5dee8814161fe5c7
Brittoexe <leoribeiro201914e@gmail.com>
2026-04-02 23:33:47 -0300

    feat: correcoes de infraestrutura
    - Alteracao do caminho do banco para relativo usando user.dir
    - Ajuste do dialeto Hibernate 7 para compatibilidade com Firebird 5.0
    - Refatoracao da DatabaseConfig para leitura dinamica de propriedades
    - Limpeza de artefatos antigos e correcao de erro de I/O

--- Backend/src/main/java/backend/config/DatabaseConfig.java
+++ Backend/src/main/java/backend/config/DatabaseConfig.java
@@ -1,54 +1,34 @@
 package backend.config;
 
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import backend.service.ConfigService;
 
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
     @Bean
-    @Primary
-    public DataSource dataSource(ConfigService configService) {
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.firebirdsql.jdbc.FBDriver");
-        
-        String path = configService.getDbPath();
-        dataSource.setUrl("jdbc:firebirdsql://localhost:3050/" + path.replace("\\", "/") + "?encoding=UTF8");
-        
-        dataSource.setUsername("SYSDBA");
-        dataSource.setPassword("1811");
-        
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("backend.model");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.community.dialect.FirebirdDialect");
-        
-        properties.setProperty("hibernate.temp.use_jdbc_metadata_defaults", "false");
-        
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.show_sql", "true");
-        
-        em.setJpaProperties(properties);
-        return em;
     }
 }

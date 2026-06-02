3804d1c461e86f6d696a7ff4bffebb5d5a25d9e0
jenifer3105 <jenifermsilva6@gmail.com>
2026-06-01 21:18:34 -0300

    Feat: melhorias de layout:

--- Frontend/src/app/pages/components/opcoes/vendas/componentsVendas/cards-indicadores/cards-indicadores.ts
+++ Frontend/src/app/pages/components/opcoes/vendas/componentsVendas/cards-indicadores/cards-indicadores.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class CardsIndicadores {
   cards = [
-    { titulo: 'VENDAS HOJE', valor: 25256.88, porcentagem: '+5,23%', positivo: true },
-    { titulo: 'VENDAS ESTE MÊS', valor: 45543.05, porcentagem: '+12,55%', positivo: true },
-    { titulo: 'VENDAS EM ANDAMENTO', valor: 32, porcentagem: '+10,50%', positivo: true, isCount: true },
-    { titulo: 'VENDAS ATRASADA', valor: 5, porcentagem: '-2,02%', positivo: false, isCount: true }
+    { titulo: 'VENDAS HOJE', valor: 'R$ 25.256,88', porcentagem: '5,23%', positivo: true, variacao: '5,23%' },
+    { titulo: 'VENDAS ESTE MÊS', valor: 'R$ 45.543,05', porcentagem: '12,55%', positivo: true, variacao: '12,55%' },
+    { titulo: 'VENDAS EM ANDAMENTO', valor: '32', porcentagem: '10,50%', positivo: true, variacao: '10,50%' },
+    { titulo: 'VENDAS ATRASADA', valor: '5', porcentagem: '-2,02%', positivo: false, variacao: '-2,02%' }
   ];
 }
